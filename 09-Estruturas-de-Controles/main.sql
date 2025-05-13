841c85d03e2b430b1dbf1ced46e8671d4bfd7ce7
Bispo Joao harv <joaohenriqueavieira@gmail.com>
2025-05-13 09:23:01 -0300

    Estrutura de Dados

--- 09-Estruturas-de-Controles/main.sql
+++ 09-Estruturas-de-Controles/main.sql
@@ -158,3 +158,99 @@ BEGIN
 END;
 /
 
+
+-- O script abaixo trabalha com a instrução FOR LOOP 
+-- e exibir 5 formas de pagamento da tabela DB_FORMA_PAGAMENTO do projeto DBurger.
+-- Após isso, irá sair do LOOP.
+SET SERVEROUTPUT ON;
+DECLARE
+    V_CONTADOR_TOTAL PLS_INTEGER;
+BEGIN
+    -- Armazena a quantidade de linhas cadastradas na tabela forma pagto.
+    SELECT COUNT(*) INTO V_CONTADOR_TOTAL FROM DB_FORMA_PAGAMENTO;
+        
+	FOR xpto IN 1..V_CONTADOR_TOTAL
+	LOOP
+	
+      DBMS_OUTPUT.PUT_LINE('Sequência (' || xpto || ')' );  
+      
+	  EXIT WHEN xpto > 4;
+	
+    END LOOP;
+END;
+/
+
+
+-- O script abaixo utiliza o conceito do comando NULL
+SET SERVEROUTPUT ON;
+DECLARE
+    v_qt_pedidos_cliente PLS_INTEGER := 00;
+    v_nr_cliente db_cliente.nr_cliente%type := 00;
+BEGIN
+
+	IF v_qt_pedidos_cliente > 10 then
+		update DB_CLIENTE SET QT_ESTRELAS = 5
+		WHERE NR_CLIENTE = V_NR_CLIENTE;
+	ELSE
+		dbms_output.put_line('Vai passar aqui e não irá fazer nada!');
+		NULL;
+	END IF;
+END;
+/
+
+
+-- O script abaixo cria variáveis e faz acesso a dados no projeto DBurger
+-- para que o processamento do exercício guiado seja realizado com sucesso
+-- PORÉM CERTIFIQUE-SE DE QUE EXISTA UM CÓDIGO DE CLIENTE VÁLIDO, pois caso contrário,
+-- esse script irá ocorrer erro de execução, sendo necessário criar controle de exceção,
+-- o que não é nosso foco nesse momento.
+SET SERVEROUTPUT ON;
+DECLARE
+    -- Variável que irá receber o número do cliente por isso que utilizamos o %type
+    v_nr_cliente db_cliente.nr_cliente%type;
+	-- Variável que irá receber o número de estrelas do cliente
+	v_qt_estrelas db_cliente.qt_estrelas%type;
+    -- Variável que irá receber o valor do ticket médio de compra do cliente
+    v_vl_ticket_medio number(10,2);
+BEGIN
+    v_nr_cliente := &informe_numero_cliente;
+
+    select  round(avg(p.vl_tot_pedido),2)
+    into    v_vl_ticket_medio
+    from    db_pedido p inner join db_cliente c
+    on  (c.nr_cliente = p.nr_cliente)
+    where   c.nr_cliente = v_nr_cliente
+    group by c.nm_cliente;
+ 
+    -- Agora vamos calcular a quantidade de estrelas
+	-- a partir do valor médio de vendas.
+	IF v_vl_ticket_medio > 399 THEN
+	   v_qt_estrelas := 5;
+	ELSIF v_vl_ticket_medio > 360 THEN
+		v_qt_estrelas := 4;
+	ELSIF v_vl_ticket_medio > 320 THEN
+		v_qt_estrelas := 3;
+	ELSIF v_vl_ticket_medio > 280 THEN
+		v_qt_estrelas := 2;
+	ELSE
+		v_qt_estrelas := 1;
+	END IF;
+
+    -- Após trabalharmos com o comando IF ELSIF ELSE END IF
+	-- chegamos a um novo valor de estrelas.
+    UPDATE 	DB_CLIENTE SET QT_ESTRELAS = V_QT_ESTRELAS
+	WHERE	NR_CLIENTE = V_NR_CLIENTE;
+    
+    -- Confirmamos a transação pendente
+	COMMIT;
+    
+    dbms_output.put_line('O cliente número(' || v_nr_cliente || ') ' 
+    || ' tem o valor do ticket médio em R$:' || v_vl_ticket_medio
+    || chr(13) || 'e o novo número de estrelas é (' || v_qt_estrelas || ')');
+
+END;
+/
+
+
+
+
